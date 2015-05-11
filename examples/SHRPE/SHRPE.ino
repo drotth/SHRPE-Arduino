9e1d8c6ef27d36ec5caa2a86d51f8f8355f81de3
Drotth <drotth@hotmail.com>
2015-05-11 13:45:47 +0200

    fast commit for soded to test code

--- examples/SHRPE/SHRPE.ino
+++ examples/SHRPE/SHRPE.ino
@@ -14,6 +14,7 @@ void loop()
 {
   Serial.write(19); //dummy to notice shield we're about to send
   delay(10);
+  incoming = 0;
   
   //uint8_t array[10] = {9, 0,1,2,3,4,5,6,7,8};
   //Serial.write(array,10);
