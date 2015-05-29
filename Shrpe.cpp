dd783d194116c3a7f88d1150c89ea5f7b43f7e02
Drotth <drotth@hotmail.com>
2015-05-29 15:34:23 +0200

    New IRQ RX branch based of master.

--- Shrpe.cpp
+++ Shrpe.cpp
@@ -13,7 +13,11 @@
 
 void shrpe_irq_handler();
 
+volatile bool dataAvailable = false;
+
 Framing framing;
+byte input_buff[100];
+int input_length, crc_valid;
 
 Shrpe::Shrpe()
 {
@@ -25,6 +29,11 @@ void Shrpe::begin()
   delay(6000); // wait for the shield to boot up
   Serial.begin(115200);
   framing.setTimout(0.1);
+  pinMode(2, INPUT); //
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  digitalWrite(2, HIGH); // turn on pull-up resistor
+  attachInterrupt(0, shrpe_irq_handler, FALLING);
 }
 
 void Shrpe::write(uint8_t data_byte)
@@ -36,4 +45,26 @@ void Shrpe::write(uint8_t data_byte)
 void Shrpe::write(uint8_t array[], uint8_t size)
 {
   framing.sendFramedData(array, size);
+}
+
+bool Shrpe::available()
+{
+	return dataAvailable;
+}
+
+void shrpe_irq_handler()
+{
+  digitalWrite(13, !digitalRead(13)); // toggle LED to indicate IRQ trigger
+  int bytes_available = Serial.available();
+		
+    if (bytes_available)
+	{
+	  framing.receiveFramedData(input_buff, input_length, crc_valid);
+	  for(int i = 0; i < input_length; i++)
+	  {
+		Serial.print(input_buff[i],DEC);
+		Serial.print(" ");
+	  }
+	Serial.println();
+	}
 }
--- Shrpe.h
+++ Shrpe.h
@@ -1,5 +1,5 @@
 /*
-  Shrpe.cpp
+  Shrpe.h
   - Library for usage together with the SHRPE shield.
   Authors: Andreas Drotth & Soded Alatia, 
   Created Date: May 28, 2015.
@@ -14,22 +14,26 @@ class Shrpe
 {
   public:
 	/*
-		Constructor för a Shrpe object.
+	  Constructor för a Shrpe object.
 	*/
     Shrpe();
 	/*
-		Initializes the Shrpe object and makes it ready for transmission of data.
+	  Initializes the Shrpe object and makes it ready for transmission of data.
 	*/
 	void begin();
 	/*
-		Writes a single (unsigned) byte to the SHRPE Shield.
+	  Writes a single (unsigned) byte to the SHRPE Shield.
 	*/
 	void write(uint8_t data_byte);
 	/*
-		Writes an array of (unsigned) bytes to the SHRPE Shield.
-		The function is capable of sending between 1-40 bytes.
+	  Writes an array of (unsigned) bytes to the SHRPE Shield.
+	  The function is capable of sending between 1-40 bytes.
 	*/
 	void write(uint8_t array[], uint8_t size);
+	/*
+	  Returns true if the shield has available data, otherwise false.
+	*/
+	bool available();
 };
 
 #endif
